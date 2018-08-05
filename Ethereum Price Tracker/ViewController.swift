0b2bf1604dd7f6cbb988c9c7310d02da4c41aaff
Conner Alegre <cga0@pm.me>
2018-08-05 11:17:15 -0600

    setup labels to reflect real prices via API

--- Ethereum Price Tracker/ViewController.swift
+++ Ethereum Price Tracker/ViewController.swift
@@ -9,16 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet var jpyLabel: UILabel!
+    @IBOutlet var eurLabel: UILabel!
+    @IBOutlet var usdLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        getPrice()
     }
 
     func getPrice() {
-        guard let url = URL(string: "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,EUR,CNY") else { return }
+        guard let url = URL(string: "https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,EUR,JPY") else { return }
         URLSession.shared.dataTask(with: url) { (data, response, error) in
-            
-        }
+            if let data = data {
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Double]
+                    guard let jsonData = json else { return }
+                    DispatchQueue.main.async {
+                        guard let ethToUSD = jsonData["USD"] else { return }
+                        guard let ethToEUR = jsonData["EUR"] else { return }
+                        guard let ethToJPY = jsonData["JPY"] else { return }
+                        
+                        self.usdLabel.text = "$\(ethToUSD)"
+                        self.eurLabel.text = "€\(ethToEUR)"
+                        self.jpyLabel.text = "¥\(ethToJPY)"
+                    }
+                } catch {
+                    NSLog("\(error)")
+                }
+            } else {
+                NSLog("\(String(describing: error))")
+            }
+        }.resume()
     }
 
 }
